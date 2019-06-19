506230a6c4f4067fce968d3eebd3e15328706940
cy <lcy7747@gmail.com>
2019-06-19 20:00:11 +0900

    clone 에서 작업

--- src/main/java/kr/or/ddit/elecAuthorization/controller/approval/ApprovalCreateController.java
+++ src/main/java/kr/or/ddit/elecAuthorization/controller/approval/ApprovalCreateController.java
@@ -3,6 +3,7 @@ package kr.or.ddit.elecAuthorization.controller.approval;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+// clone 수정
 @Controller
 public class ApprovalCreateController {
 	@RequestMapping("/elecAuthorization/approval/approInsert")
